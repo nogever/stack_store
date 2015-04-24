4ef0ee75b8d23ff554798a2a088a71a837a3fcab
lei-clearsky <sportzhulei@gmail.com>
2015-04-23 21:08:54 -0400

    get coffee and tea states

---
+++ browser/js/tea/tea.js
@@ -0,0 +1,36 @@
+'use strict';
+app.config(function ($stateProvider) {
+	$stateProvider.state('tea', {
+		url:'/tea',
+		templateUrl: 'js/tea/tea.html',
+		controller: 'TeaCtrl',
+		resolve: {
+			teaProductsInfo: function (DrinkFactory) {
+				return DrinkFactory.getTeaProducts('tea');
+			}
+		}
+	});
+});
+
+app.factory('DrinkFactory', function ($http) {
+	return {
+		getTeaProducts: function(category) {
+
+			var queryParams = {};
+
+			if (category) {
+				queryParams.category = category;
+			}
+
+			return $http.get('/api/products', {
+				params: queryParams
+			}).then(function(response) {
+				return response.data
+			});
+		}
+	};
+});
+
+app.controller('TeaCtrl', function ($scope, teaProductsInfo) {
+	$scope.teaProductsInfo = teaProductsInfo;
+});
