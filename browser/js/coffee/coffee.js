0bc57d8d4e91ae5ba2622aad188dcc4b9d18199a
lei-clearsky <sportzhulei@gmail.com>
2015-04-29 18:43:55 -0400

    added and displayed new reviews, products list and single product display works

--- browser/js/coffee/coffee.js
+++ browser/js/coffee/coffee.js
@@ -1,37 +1,83 @@
 'use strict';
 app.config(function ($stateProvider) {
+  $stateProvider.state('coffee', {
+    url:'/coffee',
+    templateUrl: 'js/coffee/coffee.html',
+    controller: 'CoffeeCtrl',
+  });
+  $stateProvider.state('coffee.home', {
+    url:'/home',
+    templateUrl: 'js/coffee/home.html',
+    controller: 'CoffeeHomeCtrl'
+  });
+  $stateProvider.state('coffee.products', {
+    url:'/products/:categoryName',
+    templateUrl: 'js/products/products-list.html',
+    controller: 'CoffeeListCtrl'
+  });
+  $stateProvider.state('coffee.product', {
+    url: '/product/:id',
+    templateUrl: 'js/products/product.html',
+    controller: 'ProductCtrl' // ProductCtrl?
+  });
 
-	$stateProvider.state('coffee', {
-		url:'/coffee',
-		templateUrl: 'js/coffee/coffee.html',
-		controller: 'CoffeeCtrl',
-		resolve: {
-			coffeeProductsInfo: function (CoffeeFactory) {
-				return CoffeeFactory.getCoffeeProducts('coffee');
-			}
-		}
-	});
 });
 
-app.factory('CoffeeFactory', function ($http) {
-	return {
-		getCoffeeProducts: function(category) {
-
-			var queryParams = {};
-
-			if (category) {
-				queryParams.category = category;
-			}
-
-			return $http.get('/api/products', {
-				params: queryParams
-			}).then(function(response) {
-				return response.data;
-			});
-		}
-	};
+app.controller('CoffeeCtrl', function ($scope) {
+  var categories = ['coffee', 'decaf', 'ice', 'americano', 'espresso', 'freshly brewed coffee'];
+  $scope.categories = categories;
 });
 
-app.controller('CoffeeCtrl', function ($scope, coffeeProductsInfo) {
-	$scope.coffees = coffeeProductsInfo;
+app.controller('CoffeeHomeCtrl', function ($scope) {
+
+});
+
+app.controller('CoffeeListCtrl', function ($scope, DrinkProductsFactory, $stateParams) {
+    
+  var categoryName = $stateParams.categoryName;
+
+  var typeName = 'coffee';
+
+  if (categoryName === 'coffee'){
+    categoryName = null;
+  }
+
+  DrinkProductsFactory.getProducts(categoryName, typeName).then(function (products) {
+    $scope.products = products;
+  }).catch(function(err) {
+    $scope.error = err;
+  });
+
+});
+
+app.controller('CoffeeProductCtrl', function ($scope, DrinkProductFactory, ProductReviewsFactory, $stateParams) {
+  DrinkProductFactory.getProduct().then(function(data) {
+
+        $scope.product = data;
+
+    });
+
+    ProductReviewsFactory.getReviews().then(function(data) {
+      $scope.reviews = data;
+    });
+
+
+    $scope.newOptions = {
+        sweets: null,
+        milk: null,
+        flavors: null, 
+        size: null,
+        toppings: null,
+    };
+
+    $scope.submit = function() {
+        console.log('new options: ', $scope.newOptions);
+
+        $http.put("api/products/" + data.id, $scope.newOption)
+        .then (function(response) {
+            console.log('hi');
+        }).catch(function(err) {
+            console.log('err');
+        });
+    }
 });
