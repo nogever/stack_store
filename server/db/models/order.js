9495344c1fe6bf7e90ca7d286bbee0d6aa67b2d0
Sean Kim <nogever@gmail.com>
2015-05-03 10:19:40 -0400

    added autonumber generator function to order model (not tested)

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -5,7 +5,7 @@ var optionsSchema = require('./options');
 var ProductModel = mongoose.model('Product');
 
 var schema = new mongoose.Schema({
-	orderNumber: {type: Number, unique: true},
+	orderNumber: {type: Number, unique: true, index: true},
 	products: [{
 		productId: { 
 			type: mongoose.Schema.Types.ObjectId, 
@@ -27,7 +27,9 @@ var schema = new mongoose.Schema({
 		]
 	},
 	_user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
-});
+}
+ //, { autoIndex: false } recommended to be added in production by the Mongoose document
+ );
 
 schema.methods.getPrice = function() {
 	if (this.products.length) {
@@ -59,6 +61,37 @@ schema.methods.populateProducts = function(order) {
 
 };
 
+function sequenceGenerator(name){
+  var SequenceSchema, Sequence;
+
+  SequenceSchema = new mongoose.Schema({
+    nextSeqNumber: { type: Number, default: 1 }
+  });
+
+  Sequence = mongoose.model(name + 'Seq', SequenceSchema);
+
+  return {
+    next: function(callback){
+      Sequence.find(function(err, data){
+        if(err) throw(err);
+        if(data.length < 1){
+          Sequence.create({}, function(err, seq){
+            if(err) throw(err);
+            callback(seq.nextSeqNumber);
+          });
+        } else {
+          Sequence.findByIdAndUpdate(data[0]._id, { $inc: { nextSeqNumber: 1 } }, function(err, seq){
+            if(err) { throw(err); }
+            callback(seq.nextSeqNumber);
+          });
+        }
+      });
+    }
+  };
+}
+
+var sequence = sequenceGenerator('order');
+
 
 schema.pre('save', function(next) {
 	var currentDate = new Date();
@@ -66,11 +99,15 @@ schema.pre('save', function(next) {
 	next();
 });
 
+schema.pre('save', function(next){
+  var doc = this;
+  sequence.next(function(nextSeq){
+    doc.orderNumber = nextSeq;
+    next();
+  });
+});
+
 mongoose.model('Order', schema);
-
-
-
-
 
 
 
